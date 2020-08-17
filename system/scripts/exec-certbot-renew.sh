be115583ac15bf6d50294d91b4762739b57aa6b4
gavin <gavin@fakemail.com>
2020-08-17 19:07:20 +0200

    changed certbot renew script to run as a different user

--- system/scripts/exec-certbot-renew.sh
+++ system/scripts/exec-certbot-renew.sh
@@ -3,22 +3,20 @@
 #purpose
 ##################
 
+#run as root
 #check for certificate renewals from letsencrypt
 #1) stop blog stack
 #2) sleep 60s
 #3) run renewal check
 #4) sleep 60s
-#5) start blog stack
+#5) ensure ownership is unchanged from update
+#6) start blog stack
 
 #cronjob for weekly renewal checks
-#0 4 * * 0 source /home/docker-user/.profile; /opt/docker-stacks/blog/system/scripts/exec-certbot-renew.sh >> /opt/docker-stacks/blog/system/logs/certbot.log 2>&1
+#0 4 * * 0 /opt/docker-stacks/blog/system/scripts/exec-certbot-renew.sh >> /opt/docker-stacks/blog/system/logs/certbot.log 2>&1
 
 #main
 ##################
-
-#0) 
-DATE=$(date +"%d_%b_%y_%H-%M-%S" | tr [a-z] [A-Z])
-echo $DATE
 
 #1)
 docker stack rm blog
@@ -27,8 +25,7 @@ docker stack rm blog
 sleep 60
 
 #3)
-#the usual t option is only for tty sessions
-docker run -i --rm -p 80:80 --name certbot \
+docker run -it --rm -p 80:80 --name certbot \
            -v "/opt/docker-stacks/blog/persistance/shared/security/letsencrypt/etc:/etc/letsencrypt" \
            -v "/opt/docker-stacks/blog/persistance/shared/security/letsencrypt/lib:/var/lib/letsencrypt" \
            certbot/certbot renew
@@ -37,4 +34,7 @@ docker run -i --rm -p 80:80 --name certbot \
 sleep 60
 
 #5)
-docker stack deploy blog -c /opt/docker-stacks/blog/deploy/docker-compose.yml
+chown -R docker-user:docker-user /opt/docker-stacks/blog/persistance/shared/security/letsencrypt/etc
+
+#6)
+/bin/su -c "docker stack deploy blog -c /opt/docker-stacks/blog/deploy/docker-compose.yml" - docker-user
