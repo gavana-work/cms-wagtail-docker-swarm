10df76cc5519e1aa0c77f8b4a3aca6b1d827d78b
gavin <gavin@fakemail.com>
2020-10-24 10:28:50 -0700

    corrected app image build process

--- build/app/build-image.sh
+++ build/app/build-image.sh
@@ -6,16 +6,24 @@ echo
 echo
 echo "[removing previous image builds]"
 echo
+	echo "[<none>]"
 	docker image rm $(docker images | grep "<none>" | awk '{print $3}') --force
+	echo "[app]"
 	docker image rm $(docker images | grep wagtail | awk '{print $3}') --force
-	docker image rm $(docker images | grep base | awk '{print $3}') --force
 
 ###########
-#BUILD
+#APP BUILD
 ###########
 
 echo
 echo
-echo "[building new base image]"
+echo "[building new app base image]"
 echo
+	cd code-base
 	docker build -t app-base .
+
+echo
+echo
+echo "[building new app image]"
+echo
+	docker build -t wagtail-custom .
